dee7e75476d3719364f3dbc93610921b32ee55e0
Chris Knight <cknight1234@gmail.com>
2021-12-08 22:05:18 +0000

    refactor: code cleanup

--- day7/day7.ts
+++ day7/day7.ts
@@ -1,16 +1,14 @@
 export function costCalcPart1(start: number[]): number {
-  let minCost = -1;
+  let minCost = Infinity;
   for (let i = Math.min(...start); i < Math.max(...start); i++) {
     const cost = start.reduce((acc, curr) => acc += Math.abs(curr - i), 0);
-    if (minCost === -1 || cost < minCost) {
-      minCost = cost;
-    }
+    minCost = Math.min(minCost, cost);
   }
   return minCost;
 }
 
 export function costCalcPart2(start: number[]): number {
-  let minCost = -1;
+  let minCost = Infinity;
   const fuelCost = (horizontalMove: number): number =>
     horizontalMove * (horizontalMove + 1) / 2;
   for (let i = Math.min(...start); i < Math.max(...start); i++) {
@@ -18,9 +16,7 @@ export function costCalcPart2(start: number[]): number {
       (acc, curr) => acc += fuelCost(Math.abs(curr - i)),
       0,
     );
-    if (minCost === -1 || cost < minCost) {
-      minCost = cost;
-    }
+    minCost = Math.min(minCost, cost);
   }
   return minCost;
 }
